325117e361e26e4949ec2e8f53d8d27433538530
dchavours <dchavours@example.com>
2021-11-19 23:14:48 -0600

    No errors

--- Source/FindKey.h
+++ Source/FindKey.h
@@ -13,3 +13,29 @@
 // The goal of this file is to keep the memory address in storage. 
 
 
+#include "libkeyfinder/keyfinder.h"
+#include "libkeyfinder/audiodata.h"
+
+
+
+// // Static because it retains useful resources for repeat use
+// static KeyFinder::KeyFinder k;
+ 
+// // Build an empty audio object
+// KeyFinder::AudioData a;
+ 
+// // Prepare the object for your audio stream
+// a.setFrameRate(yourAudioStream.framerate);
+// a.setChannels(yourAudioStream.channels);
+// a.addToSampleCount(yourAudioStream.length);
+ 
+// // Copy your audio into the object
+// for (int i = 0; i < yourAudioStream.length; i++) {
+//   a.setSample(i, yourAudioStream[i]);
+// }
+ 
+// // Run the analysis
+// KeyFinder::key_t key = k.keyOfAudio(a);
+ 
+// // And do something with the result
+// doSomethingWith(key);
